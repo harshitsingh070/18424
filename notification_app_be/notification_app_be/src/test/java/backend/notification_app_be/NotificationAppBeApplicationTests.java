e595c2040ab756894b7c31efa67b76ed3369d7d0
Harshit Singh <harshitsingh2807@gmail.com>
2026-05-04 10:39:02 +0530

    setup

---
+++ notification_app_be/notification_app_be/src/test/java/backend/notification_app_be/NotificationAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.notification_app_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
