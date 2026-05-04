45e68feed6023afc8ef9a42fb9470ccbe747bdbd
Harshit Singh <harshitsingh2807@gmail.com>
2026-05-04 11:37:53 +0530

    main app chnages

--- notification_app_be/notification_app_be/src/main/java/backend/notification_app_be/NotificationAppBeApplication.java
+++ notification_app_be/notification_app_be/src/main/java/backend/notification_app_be/NotificationAppBeApplication.java
@@ -2,12 +2,20 @@ package backend.notification_app_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import backend.notification_app_be.service.NotificationService;
 
 @SpringBootApplication
 public class NotificationAppBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationAppBeApplication.class, args);
+		ApplicationContext context = SpringApplication.run(NotificationAppBeApplication.class, args);
+		
+		NotificationService notificationService = context.getBean(NotificationService.class);
+		System.out.println("Starting");
+		notificationService.syncNotifications();
+		System.out.println("Notification are listed ");
 	}
 
 }
